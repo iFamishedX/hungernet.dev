225967142682647aaa3cfef919dd75912348a833
iFamished <me@ifamished.com>
2026-07-08 23:48:49 +0000

    Aurora

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom"
 
 // Shared UI package styles
 import "ifamished-ui/styles/index.css"
-import "ifamished-ui/styles/themes/premium/nebula.css"
+import "ifamished-ui/styles/themes/premium/aurora.css"
 // Dream
 
 // Site-specific styles
